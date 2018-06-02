1a960f77ef4a09eea9605e9391b5a419678e642f
haruhiko28 <not.hisano.soccer@gmail.com>
2018-06-02 15:49:49 +0900

    second commit

--- 01.py
+++ 01.py
@@ -1,1 +1,37 @@
-import numpy as np
+
+"""問題00:[文字列の逆順]文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．"""
+print("Do you input? y or n")
+deci = input()
+if deci == 'y':
+    print('「stressed」と入力してください')
+    moji = input()
+elif deci == "n":
+    moji = 'stressed'
+
+def rev_func(moji):
+    moji = moji[::-1]
+    return moji
+
+moji = rev_func(moji)
+print(moji)
+
+"""================================================================================"""
+
+"""問題01. 「パタトクカシーー」「パタトクカシーー」という文字列の1,3,5,7文字目を取り出して連結した文字列を得よ．"""
+print("Do you input? y or n")
+deci = input()
+
+if deci == 'y':
+    print('「パタトクカシーー」と入力してください')
+    moji = input()
+elif deci == "n":
+    moji = 'パタトクカシーー'
+
+def patotaku(moji):
+    pato = []
+    for i in [1,3,5,7]:
+        pato.append(moji[i])
+    return pato
+
+moji = patotaku(moji)
+print(moji)
