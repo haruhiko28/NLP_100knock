79818dd26e232edca6e9b482a8e9b04d63544bdf
haruhiko28 <not.hisano.soccer@gmail.com>
2018-06-05 14:41:00 +0900

    一応1章は終了

--- 01.py
+++ 01.py
@@ -1,4 +1,5 @@
-import numpy as np
+import re
+import random
 
 #================================================================================
 
@@ -23,7 +24,7 @@ def patotaku(moji):
     pato = []
     for i in [1,3,5,7]:
         pato.append(moji[i])
-    return pato
+    return ''.join(pato)
 
 moji = patotaku(moji)
 print('=> '+str(moji))
@@ -37,8 +38,6 @@ moji2 = 'タクシー'
 
 def def_02(moji1, moji2):
     _ = []
-    #moji1 = moji1.split("")
-    #moji2 = moji2.split("")
 
     for (a, b) in zip(moji1, moji2):
         _.append(a)
@@ -47,6 +46,7 @@ def def_02(moji1, moji2):
     print('=> '+str(_))
 
 def_02(moji1, moji2)
+
 #================================================================================
 
 print("\n ---03. 円周率--- ")
@@ -69,41 +69,84 @@ print("\n ---04. 元素記号--- ")
 moji = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 
 def def_04(moji):
-    moji = moji.replace(",","")
-    moji = moji.replace(".","")
+    moji = re.sub('[!@#$_“”¨«»®´·º½¾¿¡§£₤‘’.,]', '', moji)
     moji = moji.split(" ")
-
-    #print(moji)
-
     list_1 = [1,5,6,7,8,9,15,16,19]
     dic = {}
     for ix, word in enumerate(moji):
         if ix + 1 in list_1:
-            #print(ix, word[0])
             dic[word[0]] = ix + 1
         else:
-            #print(ix, word[0:2])
             dic[word[0:2]] = ix + 1
 
     print('=> '+str(dic))
 
 def_04(moji)
+
 #================================================================================
-'''
+
 print("\n ---05. n-gram--- ")
 
-def def_05(moji, ):
-'''
+class N_gram():
+    def __init__(self, sentence, n, splitter = "-*-"):
+        self.sentence = re.sub('[!@#$_“”¨«»®´·º½¾¿¡§£₤‘’.,]', '', sentence)
+        self.n = n
+        self.splitter = splitter
+
+    def word(self):
+        # make list
+        sentence_list = self.sentence.split(" ")
+
+        lst = []
+        for i in range(len(sentence_list[:-self.n+1])):
+            lst.append(self.splitter.join(sentence_list[i:i+self.n]))
+
+        return lst
 
 
+    def alpha(self):
+        # concat all alphabats
+        self.sentence = self.sentence.replace(" ","")
 
+        lst = []
+        for i in range(len(self.sentence[:-self.n+1])):
+            lst.append(self.splitter.join(self.sentence[i:i+self.n]))
+
+        return lst
+
+
+a = N_gram("I am an NLPer", 3)
+
+print("I am an NLPer")
+print(a.word())
+print(a.alpha())
+
+#================================================================================
+
+print("\n ---06. 集合--- ")
+
+b = N_gram("paraparaparadise",2,"-")
+c = N_gram("paragraph",2,"-")
+
+b1 = b.alpha()
+c1 = c.alpha()
+
+print('積集合' + str(set(b1).intersection(set(c1))))
+print('差集合' + str((set(b1).difference(set(c1)))))
+print('和集合' + str((set(b1).union(set(c1)))))
+
+if 's-e' in b1:
+    print("X has se")
+
+if 's-e' in c1:
+    print('Y has se')
 
 #================================================================================
 
 print("\n ---07. テンプレートによる文生成--- ")
 
 def def_07(x, y, z):
-    return str(x) + '時の' + str(y) +'は' + str(z)
+    return str(x) + '時の' + str(y) + 'は' + str(z)
 
 moji = def_07(12, '気温', 22.4)
 print(moji)
@@ -119,12 +162,38 @@ def def_08(bun):
             _.insert(ix, chr(219-ord(word)))
         else:
             _.insert(ix, word)
-    print(''.join(_))
+    print( '=>' + str(''.join(_)))
 
+print('paraparaparadise')
 def_08('paraparaparadise')
 
 #================================================================================
 
 print("\n ---09. Typoglycemia--- ")
 
-def def_09():
+def def_09(sentence):
+    print(sentence)
+    sentence = sentence.split(" ")
+    _ = []
+
+    for word in sentence:
+        if len(word) > 4:
+            word = list(word)
+
+            first = word[0]
+            last = word[-1]
+            middle = word[1:- 1]
+
+            random.shuffle(middle)
+            middle = ''.join(middle)
+
+            word = first + middle + last
+
+        _.append(word)
+
+    _ = ' '.join(_)
+
+    print('=>' + str(_))
+
+sentence = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+def_09(sentence)
