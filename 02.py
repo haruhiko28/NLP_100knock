28661584affab6ee817e9912e06f7c76921dee18
haruhiko28 <not.hisano.soccer@gmail.com>
2018-06-11 14:00:50 +0900

    問題ちゃんと読むべき

--- 02.py
+++ 02.py
@@ -14,7 +14,7 @@ print("\n --- 11. タブをスペースに置換 --- ")
 
 f = open('hightemp.txt','r')
 Allf = f.read()
-print(Allf)
+print( Allf )
 
 print('\n↓↓↓↓↓↓↓↓↓↓↓( ^ω^ )( ^ω^ )スペースに変換するンゴ( ^ω^ )( ^ω^ )↓↓↓↓↓↓↓↓↓↓↓\n')
 
@@ -36,24 +36,6 @@ df['prefecture'].to_csv('col1.txt',index=False)
 df['place'].to_csv('col2.txt',index=False)
 
 
-'''
-全然やってることちゃうやんけ！！！！！！！！！！！
-ld = open("hightemp.txt")
-lines = ld.readlines()
-ld.close()
-
-f1 = open('col1.txt','a')
-f2 = open('col2.txt','a')
-
-for ix , line in enumerate(lines):
-    if ix % 2 == 0:
-        f1.write(line)
-    else:
-        f2.write(line)
-
-f1.close()
-f2.close()
-'''
 #================================================================================
 
 print("\n --- 13. col1.txtとcol2.txtをマージ --- ")
@@ -65,23 +47,6 @@ col1_2 = pd.concat([col1, col2],axis=1)
 
 col1_2.to_csv('ans_13.txt',sep='\t',index=False)
 
-'''
-f1 = open('col1.txt')
-f2 = open('col2.txt')
-
-lines1 = f1.readlines()
-lines2 = f2.readlines()
-
-f1.close()
-f2.close()
-
-ans_13 = open('ans_13.txt','a')
-
-for line1,line2 in zip(lines1, lines2):
-    ans_13.write(line1 + line2)
-
-ans_13.close()
-'''
 #================================================================================
 
 print("\n --- 14. 先頭からN行を出力 --- ")
@@ -128,6 +93,7 @@ for i in range(num_lines + 1):
 
 print("\n --- 17. １列目の文字列の異なり --- ")
 
+'''
 prefec_list = list(df['prefecture'])
 mojiretu = ','.join(prefec_list)
 mojiretu = mojiretu.replace(",","")
@@ -139,37 +105,10 @@ syugo = list(set(mojiretu_list))
 print('\n↓↓↓↓↓↓↓↓↓↓↓( ^ω^ )( ^ω^ )集合に変換するンゴ( ^ω^ )( ^ω^ )↓↓↓↓↓↓↓↓↓↓↓\n')
 
 print(syugo)
-"""
-target_line = linecache.getline('hightemp.txt', 1)
-ans_17 = target_line.split(" ")
-ans_17 = ans_17[0].replace("\n"," ")
-ans_17 = ans_17.replace("\t"," ")
-ans_17 = ans_17.replace(" ","")
-print(ans_17)
+'''
 
-alnum = []
-alpha = []
-digit = []
-decimal = []
-numeric = []
+print(df['prefecture'].unique().tolist())
 
-syugo_list = (alnum, alpha, digit , decimal, numeric)
-
-for ix in range(len(ans_17)):
-    if ans_17[ix].isalnum():
-        alnum.append(ans_17[ix])
-    if ans_17[ix].isalpha():
-        alpha.append(ans_17[ix])
-    if ans_17[ix].isdigit():
-        digit.append(ans_17[ix])
-    if ans_17[ix].isdecimal():
-        decimal.append(ans_17[ix])
-    if ans_17[ix].isnumeric():
-        numeric.append(ans_17[ix])
-
-for _ in syugo_list:
-    print(_)
-"""
 #================================================================================
 
 print("\n --- 18. 各行を3コラム目の数値の降順にソート --- ")
